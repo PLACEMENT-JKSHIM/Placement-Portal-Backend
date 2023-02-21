b5c9f8f30787e826d24e8d321de74daa367c403f
PradeepRaoK <19pradeepraokokrady@gmail.com>
2023-02-21 22:54:55 +0530

    added gallery page

--- home/urls.py
+++ home/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
 path('',views.index, name='index'),   
-path('login',views.login,name="login")
-
+path('login',views.login,name="login"),
+path('gallery',views.gallery,name="gallery")
 ]
--- home/views.py
+++ home/views.py
@@ -8,3 +8,6 @@ def index(request):
 
 def login(request):
     return render(request,"login.html")
+
+def gallery(request):
+    return render(request,"gallery.html")
