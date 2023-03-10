d0025d801977632f0c9f62e762fd0210b1b8f548
Keerthan N S <keerthanns091@gmail.com>
2023-03-09 23:36:03 -0800

    [add] teams table [update]jobs table

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Company,Jobs
+from .models import Company,Jobs,Team
 # Register your models here.
 
 admin.site.register(Company)
-admin.site.register(Jobs)
+admin.site.register(Job)
+admin.site.register(Team)
