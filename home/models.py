951de7252d4763243ece8113228b59a96ed9948e
Keerthan N S <keerthanns091@gmail.com>
2023-03-13 21:10:48 -0800

    [update] teams table

--- home/models.py
+++ home/models.py
@@ -13,11 +13,9 @@ class Team(models.Model):
     mem_name=models.CharField( max_length=60,null=False)
     mem_designation=models.CharField( max_length=60,null=False)
     mem_image = models.ImageField(upload_to='teams')
-    mem_address=models.CharField(max_length=60,null=True)
-    mem_email=models.CharField(null=True,max_length=60)
-    mem_number=models.IntegerField(null=True)
+    mem_description=models.TextField(null=True)
     def __str__(self):
-        return f"{self.mem_name}-{self.mem_designation}-{self.mem_image}"
+        return f"{self.mem_name}-{self.mem_designation}-{self.mem_image}-{self.mem_description}"
 
 class Slider(models.Model):
     slider_image=models.ImageField(upload_to='sliders')
