a48bbb7030dee4be70ea76dc2105ea77a1d06a12
Keerthan N S <keerthanns091@gmail.com>
2023-03-14 14:32:03 -0800

    [unfixed] form issue

--- administrator/forms.py
+++ administrator/forms.py
@@ -1,8 +1,14 @@
 from django.forms import ModelForm 
 from django.contrib.auth.models import User
 
-class UserForm(ModelForm):
+from home.models import Slider
 
+class UserForm(ModelForm):
     class Meta:
         model=User
         fields=['username','password']
+
+class SliderForm(ModelForm):
+    class Meta:
+        model = Slider
+        fields = ['slider_image']
--- administrator/views.py
+++ administrator/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render,redirect
 from home.models import Slider, Team
-from .forms import UserForm
+from .forms import UserForm,SliderForm
 from django.http import HttpResponse
 import json
 from student.models import Student
@@ -51,6 +51,20 @@ def addStudent(request):
     return render(request, "admininstrator/student/add.html",context={'student':form})
 
 def adminEditor(request):
+    if request.method=='POST' and request.POST.get('slider_image'):
+        form = SliderForm(request.POST)
+
+        if form.is_valid():
+            slider=form.save()
+            Slider(slider=slider).save()
+            messages.success(request, message={'message': "Image added successfully"})
+        else:
+            for field,errors in form.errors.items():
+                for error in errors:
+                    messages.error(request, message={'message':error,'title':field})
+        
+        form = SliderForm()
+
     members = Team.objects.all()
     sliders = Slider.objects.all()
-    return render(request, "admininstrator/adminEditor.html",{'members':members,'sliders':sliders})
+    return render(request, "admininstrator/adminEditor.html",{'members':members,'sliders':sliders},context={'members':form})
