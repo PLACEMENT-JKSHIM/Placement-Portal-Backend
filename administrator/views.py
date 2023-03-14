6e3e60622bc0c6bf00ab1c8d75c220b1ca90b6e6
NidheeshaT <ntbayari@gmail.com>
2023-03-14 12:25:52 +0530

    added student addition along with user and added a button component

--- administrator/views.py
+++ administrator/views.py
@@ -13,15 +13,33 @@ def index(request):
 def addStudent(request):
     if request.method=='POST' and request.POST.get('students'):
         data=json.loads(request.POST.get('students'))
-        data=[User(username=d['username'],password=d['password']) for d in data]
-        User.objects.bulk_create(data,ignore_conflicts=True)
-        messages.success(request, message={'message':"added successfully"})
+        try:
+            for d in data:
+                u,success=User.objects.get_or_create(username=d['username'],password=d['password'])
+                if not success:
+                    continue
+                u.save()
+                s=Student.objects.create(user=u)
+                s.save()
+        except:
+            messages.error(request, message={'title':"username",'message':"no column username"})
+            messages.error(request, message={'title':"password",'message':"no column password"})
+        else:
+            messages.success(request, message={'message':"added successfully"})
+
+        # data=[User(username=d['username'],password=d['password']) for d in data]
+        # users=User.objects.bulk_create(data,ignore_conflicts=True)
+        # students=[Student(user=user) for user in users]
+        # print(students)
+        # Student.objects.bulk_create(students,ignore_conflicts=True)
+
 
     elif request.method=='POST':
         form=UserForm(request.POST)
 
         if form.is_valid():
-            form.save()
+            user=form.save()
+            Student(user=user).save()
             messages.success(request, message={'message': "Saved successfully"})
         else:
             for field,errors in form.errors.items():
