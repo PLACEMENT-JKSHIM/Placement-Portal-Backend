39adb3c108f5b410cf4f07d3dead4bf3b204c1af
PradeepRaoK <19pradeepraokokrady@gmail.com>
2023-03-16 20:36:28 +0530

    added jobstudent model

--- administrator/models.py
+++ administrator/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+from home.models import Job
+from student.models import Student
+
+
+class Job_student(models.Model):
+    class Status(models.TextChoices):
+        APPLIED = 'A'
+        OFFERED = 'O'
+        REJECTED = 'R'
+        QUALIFIED = 'Q'
+    job = models.OneToOneField(Job, on_delete=models.CASCADE)
+    student = models.OneToOneField(Student, on_delete=models.CASCADE)
+    status=models.CharField(max_length=2,choices=Status.choices,default=Status.APPLIED)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    def __str__(self):
+        return f"{self.job}-{self.student}-{self.status}"
