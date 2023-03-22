5a905e2ce9e7673ca58e528e78e88c28afb1da79
Keerthan N S <keerthanns091@gmail.com>
2023-03-21 22:36:09 -0800

    [fix] register rule

--- home/admin.py
+++ home/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Company,Job,Team,Slider
+from .models import Company,Job,Team,Slider,Rule
 # Register your models here.
 
 admin.site.register(Company)
 admin.site.register(Job)
 admin.site.register(Team)
 admin.site.register(Slider)
+admin.site.register(Rule)
