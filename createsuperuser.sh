6fe0d324007ddedadd8ec4cb0c349dc122c7d966
NidheeshaT <ntbayari@gmail.com>
2023-05-27 18:45:11 +0530

    removed unneccassary code

--- createsuperuser.sh
+++ createsuperuser.sh
@@ -1,7 +1,1 @@
-set -o allexport
-test -f .env && source .env
-set +o allexport
-
-
-
 python manage.py shell -c "import environ; env = environ.Env(); env.read_env(); from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(env.get_value('SUPERUSER_USERNAME').strip(), env.get_value('SUPERUSER_EMAIL').strip(), env.get_value('SUPERUSER_PASSWORD').strip())"
